27062bdd7afcfca87ab099afe8da42b79bc7edda
jcsalerno <contato.jcsalerno@gmail.com>
2025-04-03 21:29:00 -0300

    build: configuração estrutal inicial do projeto Spring Boot

---
+++ src/test/java/br/com/techchallenge/foodsys/FoodsysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.foodsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
