504f2f43c5767842fa43fc0dd39ccb8f856dfbfb
GustavoLimaAliba <gustavo.aliba@sptech.school>
2025-04-05 23:05:13 -0300

    Criação de create usuario, configs, exception e mensagens

--- src/main/java/br/com/techchallenge/foodsys/controller/UsuarioController.java
+++ src/main/java/br/com/techchallenge/foodsys/controller/UsuarioController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final CriarUsuarioCommand criarUsuarioCommand;
-    private final MessageSource messageSource;
+
     @PostMapping
     public ResponseEntity<Void> criarUsuario(@RequestBody @Valid CriarUsuarioCommandDto criarUsuarioCommandDto) {
         criarUsuarioCommand.execute(criarUsuarioCommandDto);
