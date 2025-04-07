102c61aa2f9be631d31ca540afb26ac4825e2518
GustavoLimaAl <alibagustavo0@gmail.com>
2025-04-07 18:22:11 -0300

    Ajuste

--- src/main/java/br/com/techchallenge/foodsys/dominio/usuario/UsuarioRepository.java
+++ src/main/java/br/com/techchallenge/foodsys/dominio/usuario/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package br.com.techchallenge.foodsys.dominio.usuario;
 
+import org.springdoc.core.converters.models.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByEmail(String email);
-    boolean existsByLogin(String email);
+    boolean existsByLogin(String login);
+    List<Usuario> findByAtivoTrue(Sort sort);
 }
